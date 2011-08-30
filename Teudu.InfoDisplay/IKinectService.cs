0051a1b7f008159d72d411d20e6a42a8cbee072b
Vivek Pai <MasterPie@users.noreply.github.com>
2011-08-30 04:07:46 +0000

    - Initial check in with MVVM setup

---
+++ Teudu.InfoDisplay/IKinectService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Teudu.InfoDisplay
+{
+    public interface IKinectService 
+    { 
+        void Initialize(); 
+        void Cleanup(); 
+        event EventHandler<SkeletonEventArgs> SkeletonUpdated;    
+    }
+}
---
+++ Teudu.InfoDisplay/ViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace Teudu.InfoDisplay
+{
+    public class ViewModel: INotifyPropertyChanged
+    {
+        IKinectService kinectService; 
+        
+        public ViewModel(IKinectService kinectService) 
+        {
+            this.RightHandOffsetX = 100;
+            this.RightHandOffsetY = 100; 
+            this.kinectService = kinectService; 
+            this.kinectService.SkeletonUpdated += new System.EventHandler<SkeletonEventArgs>(kinectService_SkeletonUpdated); 
+        }        
+        
+        void kinectService_SkeletonUpdated(object sender, SkeletonEventArgs e) 
+        { 
+            if (App.Current.MainWindow != null) 
+            { 
+                var midpointX = App.Current.MainWindow.Width / 2; 
+                var midpointY = App.Current.MainWindow.Height / 2;
+                this.RightHandOffsetX = midpointX + (e.RightHandPosition.X * 500);
+                this.RightHandOffsetY = midpointY - (e.RightHandPosition.Y * 500);
+                this.LeftHandOffsetX = midpointX + (e.LeftHandPosition.X * 500);
+                this.LeftHandOffsetY = midpointY - (e.LeftHandPosition.Y * 500); 
+            } 
+        }        
+        
+        double rightHandOffsetX; 
+        
+        public double RightHandOffsetX 
+        {
+            get { return this.rightHandOffsetX; }
+            set { this.rightHandOffsetX = value; this.OnPropertyChanged("RightHandOffsetX"); } 
+        }        
+        
+        double rightHandOffsetY; 
+        public double RightHandOffsetY 
+        {
+            get { return this.rightHandOffsetY; }
+            set { this.rightHandOffsetY = value; this.OnPropertyChanged("RightHandOffsetY"); } 
+        }
+
+        double leftHandOffsetX;
+
+        public double LeftHandOffsetX
+        {
+            get { return this.leftHandOffsetX; }
+            set { this.leftHandOffsetX = value; this.OnPropertyChanged("LeftHandOffsetX"); }
+        }
+
+        double leftHandOffsetY;
+        public double LeftHandOffsetY
+        {
+            get { return this.leftHandOffsetY; }
+            set { this.leftHandOffsetY = value; this.OnPropertyChanged("LeftHandOffsetY"); }
+        }  
+        
+        void OnPropertyChanged(string property) 
+        { 
+            if (this.PropertyChanged != null) 
+            { 
+                this.PropertyChanged(this, new PropertyChangedEventArgs(property)); 
+            } 
+        }        
+        
+        public void Cleanup() 
+        { 
+            this.kinectService.SkeletonUpdated -= kinectService_SkeletonUpdated; 
+        }        
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
