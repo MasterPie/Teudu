1df3ec83c1c38afbf6fc5122390054d594e357f2
Vivek Pai <MasterPie@users.noreply.github.com>
2012-02-04 07:03:37 +0000

    - Event hover animation improvements

--- Teudu.InfoDisplay/Eventing/SkeletonEventArgs.cs
+++ Teudu.InfoDisplay/Eventing/SkeletonEventArgs.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Microsoft.Research.Kinect.Nui;
+using Microsoft.Kinect;
 
 namespace Teudu.InfoDisplay
 {
     public class SkeletonEventArgs : EventArgs 
     {
-        public Vector HeadPosition { get; set; }
-        public Vector ChestPosition { get; set; }
-        public Vector LeftHandPosition { get; set; }
-        public Vector RightHandPosition { get; set; }
-        public Vector LeftShoulderPosition { get; set; }
-        public Vector RightShoulderPosition { get; set; }
-        public Vector LeftElbowPosition { get; set; }
-        public Vector RightElbowPosition { get; set; }
-        public Vector SpinePosition { get; set; }
-        public Vector TorsoPosition { get; set; }
+        public SkeletonPoint HeadPosition { get; set; }
+        public SkeletonPoint ChestPosition { get; set; }
+        public SkeletonPoint LeftHandPosition { get; set; }
+        public SkeletonPoint RightHandPosition { get; set; }
+        public SkeletonPoint LeftShoulderPosition { get; set; }
+        public SkeletonPoint RightShoulderPosition { get; set; }
+        public SkeletonPoint LeftElbowPosition { get; set; }
+        public SkeletonPoint RightElbowPosition { get; set; }
+        public SkeletonPoint SpinePosition { get; set; }
+        public SkeletonPoint TorsoPosition { get; set; }
     }
 }
